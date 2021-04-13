fbf15fda6793773103310e8739b3457123b09358
Staisław Solarewicz <szahuyt@gmail.com>
2021-04-13 22:11:26 +0200

    prototype working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from "react";
-import { getCountryPopulation, getData } from "./utils";
+import React, { useState } from "react";
+import { getCountryPopulation, fetchCountriesList, populationToFormatedString, correctColor, wrongColor } from "./utils";
 import './App.css';
 
 const TitleComponent = (props) => {
-  return <p id="titleText">The country with population of <strong style={{fontSize: '33px'}}>{props.population}</strong> is:</p>
+  return <p id="titleText">The country with population of <br/> <strong style={{fontSize: '33px'}}>{props.population}</strong> is:</p>
 }
 
 const MainCard = (props) => {
+
+    const [pressedIndex, setPressedIndex] = useState(-1);
+    const [guessCasted, setGuessCasted] = useState(false);
+    const onClickCallback = (index) => {setGuessCasted(true); setPressedIndex(index);}
+
     return <div id="mainCard">
-      <TitleComponent population={props.population}/>
+      <TitleComponent key='titleComponent' population={populationToFormatedString(props.countries[0].population)}/>
+      {props.countries.map((country, i) => {
+        return <CountryButton key={country.name} onClick={onClickCallback} index={i} isCorrect={ i===props.correctOne ? true : false}
+         color={ guessCasted ? i===pressedIndex && i===props.correctOne ? correctColor : i===pressedIndex ?  wrongColor : i===props.correctOne ? correctColor : 'white' : 'white'} label={country.name}/>}
+      )}
     </div>
 }
 
+const CountryButton = (props) => {
+  return <button id="countryButton" onClick={() => props.onClick(props.index)} style={{backgroundColor: props.color}}>{props.label}</button>
+}
 
 
 class MainComponent extends React.Component {
@@ -24,14 +36,14 @@ class MainComponent extends React.Component {
       randomCountries[i] = Math.floor(Math.random() * 200);
     }
 
-    this.correctOne = Math.floor(Math.random() * 3);
+    this.correctOne = Math.floor(Math.random() * 4);
 
     this.state = {randomCountries: randomCountries, loaded: false};
   }
 
   componentDidMount() {
     
-    getData((res) => {
+    fetchCountriesList((res) => {
 
       this.setState({randomCountries: this.state.randomCountries.map((x) => {
         return {name: res[x].name, code: res[x].code, population: 0}
@@ -51,22 +63,10 @@ class MainComponent extends React.Component {
 
   }
 
-  /*
-  render() {
-    return (<div>
-      <ul>
-        {
-          this.state.loaded ? 
-           this.state.randomCountries.map((element) => {return <li key={element.code}>{element.population}</li>}) :
-          <li>loading</li>
-        }
-      </ul>
-      </div>)
-  } */
 
   render() {
     return <div>
-      <MainCard population={this.state.randomCountries[this.correctOne].population}/>
+      <MainCard countries={this.state.randomCountries} correctOne={this.correctOne}/>
       </div>;
   }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,9 @@
-export const getData = (callback) => {
+//COLORS: 
+export const correctColor = '#95D575';
+export const wrongColor = '#DE3C66';
+
+//fethching json with list of all countries
+export const fetchCountriesList = (callback) => {
     fetch('countryList.json', {headers: {
       'Content-Type': 'applicaton/json',
       'Accept': 'application/json'
@@ -11,11 +16,38 @@ export const getData = (callback) => {
     });
 }
 
+//calling worldbak api to get population by country code
 export const getCountryPopulation = function (countryCode, callback) {
-
     fetch(`https://api.worldbank.org/v2/country/${countryCode}/indicator/SP.POP.TOTL?format=json`)
     .then(res => res.json())
     .then(data => { callback(data[1][1].value)}); 
-    
+}
+
+//formatting population into readable string
+export function populationToFormatedString(population) {
+  if(population) {
+    let popString = population.toString();
+    switch(popString.length) {
+      case 9:
+        return popString.slice(0, 3) + ' million';
+      case 8:
+        return popString.slice(0, 2) + ' million';
+      case 7:
+        return popString.slice(0, 1) + ' million';
+      case 6:
+        return popString.slice(0, 3) + ' thousand';
+      case 5:
+          return popString.slice(0, 2) + ' thousand';
+      case 4:
+        return popString.slice(0, 1) + ' thousand';
+      case 3:
+        return popString.slice(0, 2) + ' thousand';
+      default:
+        console.error('something went wrong');
+    }
+  } else {
+    return 0;
+  }
+  
 }
 
